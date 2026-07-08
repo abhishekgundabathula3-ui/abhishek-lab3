96d4436a6af8a098f88b08bad5c48372c164f88a
abhishekgundabathula3-ui <abhishek.gundabathula3@gmail.com>
2026-07-08 15:50:57 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Write your code below to print "Hello World"
+        System.out.println("Hello World");
         
     }
 }
